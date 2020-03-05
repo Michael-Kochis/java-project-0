6fbcb523230c62f768b26fe78e4f814c66061501
Michael-Kochis <rhishisikk@gmail.com>
2020-03-05 13:21:17 -0500

    Added types for apply accounts to AccountType.

--- src/com/revature/enums/AccountType.java
+++ src/com/revature/enums/AccountType.java
@@ -1,11 +1,13 @@
 package com.revature.enums;
 
 public enum AccountType {
-	ACCT_CHECK, ACCT_SAVE;
+	ACCT_CHECK, ACCT_SAVE, APPLY_CHECK, APPLY_SAVE;
 	
 	public static AccountType intToType(int input) {
 		switch(input) {
 		case 1: return AccountType.ACCT_CHECK;
+		case 2: return AccountType.APPLY_SAVE;
+		case 3: return AccountType.APPLY_CHECK;
 		default: return AccountType.ACCT_SAVE;
 		}
 	}
@@ -13,6 +15,10 @@ public enum AccountType {
 	public static int typeToInt(AccountType a) {
 		if (a == AccountType.ACCT_CHECK) {
 			return 1;
+		} else if (a == AccountType.APPLY_SAVE) {
+			return 2;
+		} else if (a == AccountType.APPLY_CHECK) {
+			return 3;
 		} else {
 			return 0;
 		}
